99e24b7a0a5f134cf8f5838333806cfbf408ddf4
Daniel Torres <dtorres@mequedouno.com>
2018-05-15 16:54:00 +0200

    bp added

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,16 +1,10 @@
 from flask import current_app, g, jsonify
 from .. import models
+from .auth import *
 
 def init_app(app):
     print('routes/__init__.py init_app(app)')
 
-    @app.route('/')
-    def index():
-        
-        print (g)
-        db = models.db
-        users = models.User.query.all()
-        return jsonify(users)
-
+    app.register_blueprint(auth.bp)
 
     return app
