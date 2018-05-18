210af268226b26d0ecc83a01a911c62dd79d2d97
Daniel Torres <dtorres@mequedouno.com>
2018-05-18 14:28:13 +0200

    gravatar added

--- routes/auth/__init__.py
+++ routes/auth/__init__.py
@@ -7,6 +7,8 @@ from .. import models
 
 from . import register,login,logout
 
+from libgravatar import Gravatar
+
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 def load_logged_in_user():
@@ -16,6 +18,7 @@ def load_logged_in_user():
         g.me = None
     else:
         g.me = models.auth.User.query.filter_by(id = user_id).first()
+        g.gravatar = Gravatar(g.me.email)
 
 def login_required(view):
     @functools.wraps(view)
