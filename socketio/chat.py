6b286e47ee2f4442826226a021b96d620ceee4cf
Daniel Torres <dtorres@mequedouno.com>
2018-06-20 17:56:44 +0200

    chat namespace class: added decorators

--- socketio/chat.py
+++ socketio/chat.py
@@ -4,6 +4,15 @@ from flask_socketio import Namespace, emit
 
 from .. import models, routes
 
+def load_login_or_ignore(func):
+    def wrapped_func(*args,**kwargs):
+        routes.auth.load_logged_in_user()
+        if g.me is None:
+            return None
+
+        return func(*args,**kwargs)
+
+    return wrapped_func
 
 class Chat(Namespace):
 
@@ -24,44 +33,41 @@ class Chat(Namespace):
     '''
         On connect
     '''
+    @load_login_or_ignore
     def on_connect(self):
-        routes.auth.load_logged_in_user()
-        
-        if g.me is not None:
-            connUser = None
-            for conn in self.connectedUsers:
-                if conn['email'] == g.me.email:
-                    connUser = conn
 
-            if connUser is None:
-                self.connectedUsers.append({
-                    'userId': g.me.id,
-                    'email': g.me.email,
-                    'sid': request.sid
-                })
-            
-            self.emitUsers()
+
+        connUser = None
+        for conn in self.connectedUsers:
+            if conn['email'] == g.me.email:
+                connUser = conn
+
+        if connUser is None:
+            self.connectedUsers.append({
+                'userId': g.me.id,
+                'email': g.me.email,
+                'sid': request.sid
+            })
+        
+        self.emitUsers()
 
     
     '''
         On disconnect
     '''
+    @load_login_or_ignore
     def on_disconnect(self):
-        routes.auth.load_logged_in_user()
 
-        if g.me is not None:
-            conn = next( x for x in self.connectedUsers if x['userId'] == g.me.id)
-            self.connectedUsers.remove(conn)
-            self.emitUsers()
+        conn = next( x for x in self.connectedUsers if x['userId'] == g.me.id)
+        self.connectedUsers.remove(conn)
+        self.emitUsers()
 
     
     '''
         On custom events
     '''
+    @load_login_or_ignore
     def on_send_message_to_user(self,email,message):
-        
-        routes.auth.load_logged_in_user()
-        
-        if g.me is not None:
-            conn = next( x for x in self.connectedUsers if x['email'] == email)
-            self.emit('get_message_from_user',(g.me.email, message,), room=conn['sid'])
+
+        conn = next( x for x in self.connectedUsers if x['email'] == email)
+        self.emit('get_message_from_user',(g.me.email, message,), room=conn['sid'])
